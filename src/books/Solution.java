482c5bef1c9eeba84926b7b3c68744137ad9aff9
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-09-21 10:40:45 +0530

    add Scanner class object add for loop to get input created a object and also add Output Statements

--- src/books/Solution.java
+++ src/books/Solution.java
@@ -1,5 +1,28 @@
 package books;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner r = new Scanner(System.in);
+        int n = r.nextInt();
+
+        if (n > 0) {
+            Books[] books = new Books[n];
+            r.nextLine().trim();
+            for (int i = 0; i < n; i++) {
+                String bookName = r.nextLine().trim();
+                String authorName = r.nextLine().trim();
+                String ISBN = r.nextLine().trim();
+                books[i] = new Books(bookName, authorName, ISBN);
+            }
+            for (int i = 0; i < books.length; i++) {
+                System.out.println(books[i]);
+            }
+            r.close();
+        } else {
+            System.out.println("N/A");
+        }
+    }
 }
