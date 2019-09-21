55a8b74d3551715423666d6df7d37df7d856470c
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-09-21 10:37:40 +0530

    add private varaiables add constructor add getters and setters and add a override method

--- src/books/Books.java
+++ src/books/Books.java
@@ -1,4 +1,47 @@
 package books;
 
 public class Books {
+    private String authorName;
+    private String bookName;
+    private String ISBN;
+
+    public Books(String bookName, String authorName, String ISBN) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBN = ISBN;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    @Override
+    public String toString() {
+        return "-----------------------------\n" +
+                "Book Name:\t" + getBookName() +
+                "\nAuthor Name:\t" + getAuthorName() +
+                "\nISBN:\t" + getISBN() +
+                "\n-----------------------------";
+    }
+
 }
